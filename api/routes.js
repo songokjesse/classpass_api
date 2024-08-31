fbfe3fc89d90e60f319c1a238717b98a6e3eb1ef
Jesse Songok <jessesongok@gmail.com>
2024-08-31 13:59:47 +0300

    Refactor: route attendance

--- api/routes.js
+++ api/routes.js
@@ -3,13 +3,13 @@ const {authenticateToken} = require("./auth/authToken");
 const login = require("./auth/login");
 const register = require("./auth/register");
 const logout = require("./auth/logout");
-const {attendance} = require("./attendance/attendance");
+const attendance = require("./attendance/attendance");
 const routes = express.Router()
 
 routes.post('/login', login)
 routes.post('/register', register)
 routes.post('/logout',authenticateToken, logout)
-routes.post('/attendance',authenticateToken, attendance )
+routes.post('/attendance', attendance )
 routes.get("/user", authenticateToken, (req, res) => {
     res.json(req.user);
 });
